6e3c0ab703fa374dd96c53617d4ed5222cf771e9
BirkhoffLee <github@birkhoff.me>
2017-09-19 15:51:31 +0800

    add jquery to production

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -25,6 +25,12 @@ var webpackConfig = merge(baseWebpackConfig, {
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      "root.jQuery": "jquery"
+    }),
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
